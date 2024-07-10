5cafb7ea888aca251b2fe836df1037dd7807d417
creativeofvisualimpairmen <kreatifoffvisualempire@gmail.com>
2024-07-10 18:45:13 +0800

    koppplak

--- js/data-video.js
+++ js/data-video.js
@@ -2,4 +2,5 @@ var dataVideo = ['<a onclick="harmoniMerakit()">Harmoni Jiwa Cover Yura Yunita -
 '<a onclick="harmoniManuk()">Harmoni Jiwa Cover - Manuk Dadali</a>',
 '<A onclick="azkaUnholy()">Azka Guitar Cover - Avenged Seven Fold - Unholy Conffession</a>',
 '<a onclick="harmoniEs()">Harmoni Jiwa Cover - Es Lilin</a>',
-'<a onclick="jamilahTakInginUsai()">Jamilah dkk Cover - Keisya Levronka - Tak Ingin Usai</a>'];
+'<a onclick="jamilahTakInginUsai()">Jamilah dkk Cover - Keisya Levronka - Tak Ingin Usai</a>',
+'<a onclick="jamilahArof73()">Jamilah Al-Arof 73-77</a>'];
--- js/tampilVideo.js
+++ js/tampilVideo.js
@@ -15,3 +15,21 @@ var kontenUtama = document.getElementById("utama");
 kontenUtama.innerHTML = '<h2>SELAMAT MENONTON</h2><hr>' +
 '<iframe width="560" height="315" src="https://www.youtube.com/embed/bPp4ruz6G80?si=_cwYwfd9clCqESKM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>';
 }
+
+function harmoniEs(){
+var kontenUtama = document.getElementById("utama");
+kontenUtama.innerHTML = '<h2>SELAMAT MENONTON</h2><hr>' +
+'<iframe width="560" height="315" src="https://www.youtube.com/embed/DliD_yFTtnM?si=DTv4fWo4KCsZB8xX" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>';
+}
+
+function jamilahTakInginUsai(){
+var kontenUtama = document.getElementById("utama");
+kontenUtama.innerHTML = '<h2>SELAMAT MENONTON</h2><hr>' +
+'<iframe width="560" height="315" src="https://www.youtube.com/embed/amnDM7X2iJg?si=jvKX1qQd34nfSNSA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>';
+}
+
+function jamilahArof73(){
+var kontenUtama = document.getElementById("utama");
+kontenUtama.innerHTML = '<h2>SELAMAT MENONTON</h2><hr>' +
+'<iframe width="560" height="315" src="https://www.youtube.com/embed/WqQI7fACiWs?si=FkFSjgIBzfQzx0yd" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>';
+}
